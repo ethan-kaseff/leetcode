c05e3e6441d094138e180cfa396f7738737878f5
Ethan Kaseff <67133581+ethan-kaseff@users.noreply.github.com>
2022-06-08 23:52:12 -0500

    Time: 113 ms (5.95%), Space: 42.3 MB (27.38%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    let tres = 0;
+    
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            tres = 0;
+        } else {
+            tres ++;
+        }
+        
+        if (tres === 3) return true;
+    }
+    
+    return false;
+};
