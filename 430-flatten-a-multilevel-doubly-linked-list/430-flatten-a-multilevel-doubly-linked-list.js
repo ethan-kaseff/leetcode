4e777e28618b9e2f7c465fabd44918625a157608
Ethan Kaseff <67133581+ethan-kaseff@users.noreply.github.com>
2022-08-25 14:19:29 -0500

    Time: 123 ms (8.40%), Space: 45.8 MB (7.33%) - LeetHub

---
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.js
@@ -0,0 +1,67 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head) {
+    
+    if(head == null) return head;
+    
+    let currentNode = head; 
+    let currentChild = new Node(); 
+    
+    if (currentNode.child != null) currentChild = currentNode.child;
+
+    
+    while(currentNode != null && (currentNode.next != null || currentChild != null)) {
+        console.log('enter')
+        console.log('currentNode', currentNode.val)
+        // when current Child 
+        if (currentNode.child != null) {
+            
+            // save hold, currentChild
+            let tempHold = currentNode.next;
+            currentChild = currentNode.child; 
+            
+            // remove child
+            currentNode.child = null;
+            
+            // align next row onto main
+            currentNode.next = currentChild;
+            currentChild.prev = currentNode;
+            
+            // move through next row
+            while(currentChild.next != null) {
+                console.log('currentChild', currentChild.val);
+                currentChild = currentChild.next;
+            }
+            
+            // at end connect back floating part of main
+            if(tempHold != null) {
+                currentChild.next = tempHold;
+                tempHold.prev = currentChild;
+            }
+            
+            currentNode = currentNode.next;
+
+        } else {
+            currentNode = currentNode.next;
+        }
+        
+    }
+    
+    return head;
+    
+    
+    
+    
+};
